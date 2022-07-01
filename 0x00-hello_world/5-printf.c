97c1cf3972824cb215feed9915703befb6613330
calito112 <chisomogueji@gmail.com>
2022-06-30 21:55:31 -0700

    100-quotes.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main print a string
- * Description: use printf
+ * Description: print "with proper grammar, but the outcome is a piece of art,\n"
  * Return: 0
  */ 
 int main(void)
