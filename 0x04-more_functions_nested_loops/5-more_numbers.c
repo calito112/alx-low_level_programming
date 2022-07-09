eb3fc98c5da4f6ea6023782e9fc5e313750e089a
calito112 <chisomogueji@gmail.com>
2022-07-09 06:15:50 +0100

    prints 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* more_numbers - prints more numbers
+*/
+
+void more_numbers(void)
+{
+int i j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+_putchar(j + '0');
+}
+_putchar('\n');
+}
+}
