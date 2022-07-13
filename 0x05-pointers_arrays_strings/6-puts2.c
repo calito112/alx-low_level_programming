9034b624db5fc06d42fc92da224230f2efa4a8bf
calito112 <chisomogueji@gmail.com>
2022-07-13 23:48:04 +0100

    Hot fix!

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,10 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+int len;
 
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i += 1;
-	}
-	_putchar('\n');
+len = strlen(str);
+for (i = 0; i < len; i += 2)
+_putchar(str[i]);
+_putchar('\n');
 }
