5298db219132cd7e7a00ec62439d8e4fe68deb32
calito112 <chisomogueji@gmail.com>
2022-07-22 13:58:56 +0100

    Hot fix!

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,20 +2,21 @@
 #include <string.h>
 
 /**
- * _memset -> memory set function
+ * _memset - fills the first n bytes of the memory of the
+ *			memory area pointed to by s with constant byte b
+ *
  * @s: pointer to the string s
  * @b: char variable
  * @n: size in byte of a memory space
- * Return: pointer to the variable s
+ *
+ * Return: pointer to the variable s.
+ *
+ * Created by: Sanctus-Peter
+ * cc: 17th, july 2022.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int a;
-
-for (a = 0; a < n; a++)
-{
-s[a] = b;
-return (s);
-}
+	memset(s, b, n);
+	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,5 +14,3 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	memcpy(dest, src, n);
 	return (dest);
 }
-Footer
-
